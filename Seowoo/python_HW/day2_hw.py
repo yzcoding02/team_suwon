1551ef65239fd1718515d28bdfd6338c96232547
kimsunsang0321 <stellachoi77777@gmail.com>
2023-01-27 21:37:43 +0900

    복습!

--- Seowoo/python_HW/day2_hw.py
+++ Seowoo/python_HW/day2_hw.py
@@ -1,24 +1,24 @@
-1
-for i in range(7):
-    print("내일은 토요일이다")
-2
-for i in range(3, 11, 3):
-    print(i)
-4
-for i in range(50, 101):
-    if i % 3 == 0:
-        if i % 4 == 0:
-            print(i)
-5
-sum = 0
-mul = 1
-for i in range(1, 101):
-    if i % 2 == 0:
-        mul += i
-    if i % 2 == 1:
-        sum += i
-print(mul - sum)
-3
+# 1
+# for i in range(7):
+#     print("내일은 토요일이다")
+# 2
+# for i in range(3, 11, 3):
+#     print(i)
+# 4
+# for i in range(50, 101):
+#     if i % 3 == 0:
+#         if i % 4 == 0:
+#             print(i)
+# 5
+# sum = 0
+# mul = 1
+# for i in range(1, 101):
+#     if i % 2 == 0:
+#         mul += i
+#     if i % 2 == 1:
+#         sum += i
+# print(mul - sum)
+# 3
 sum = 0
 for i in range(1, 21):
     if i % 4 != 0:
---
+++ Seowoo/python_HW/복습.py
@@ -0,0 +1,16 @@
+sum = 0
+for i in range(20, 41):
+    if i % 3 != 0:
+        sum += i
+print(sum)
+
+for i in range(50, 101):
+    if i % 5 == 0:
+        if i % 7 == 0:
+            print(i)
+
+for i in range(1, 11):
+    if i % 2 == 1:
+            print(i)
+
+
