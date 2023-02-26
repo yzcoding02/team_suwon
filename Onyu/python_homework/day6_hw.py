4a9ab229989b6012db67abd029d0793796c6b0cb
CLaireChoi1209 <4clairechoi@gmail.com>
2023-02-26 22:23:16 +0900

    끝(이겟지???)~~

---
+++ Onyu/python_homework/day6_hw.py
@@ -0,0 +1,40 @@
+# 1번_특6,7_index, indexing
+오뉴 = [485695, 489349, 973434]
+
+print(오뉴.index(973434))
+print(오뉴[0])
+
+# 2번_특8_count
+웅스 = [4, 3, 2, 1]
+print(웅스.count(2))
+
+# 3번_특9_insert
+서웅 = [1, 2, 3]
+서웅.insert(2, 99999)
+
+# 4번_특10_remove
+영쓱 = [9997, 9998, 8888, 9999]
+영쓱.remove(8888)
+
+# 5번_특11_extend
+윶이 = [7, 6, 5]
+렝쓰 = [4, 3, 2, 1]
+윶이.extend(렝쓰)
+print(렝쓰)
+
+# 6번_특12_+연산자
+헤이 = [101, 201]
+지니 = [301, 401, 501]
+print(헤이 + 지니)
+
+# 7번_특13_len
+럭키 = [16543, 27436, 36946]
+print(len(럭키))
+
+# 8번_특14
+numbers = ["1", "2", "3"]
+for i in range(len(numbers)):
+    print(numbers[i])
+
+for number in numbers:
+    print(number)
