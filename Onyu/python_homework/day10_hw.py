d5808a90e70a55533b17b02647de768feff7eed6
CLaireChoi1209 <4clairechoi@gmail.com>
2023-04-29 10:41:40 +0900

    this is sookjae

---
+++ Onyu/python_homework/day10_hw.py
@@ -0,0 +1,18 @@
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(a[4:7])
+
+# q1
+fruits = ["banana", "orange", "apple", "peach"]
+fruits.pop()
+print(fruits[2:])
+
+# q2
+fruits = ["cherry", "pear", "grape", "mario"]
+fruits.sort()
+fruits.append("louise")
+print(fruits[2:])
+
+# q3
+fruits = ["mango", "red orange", "white orange(?)", "blue orange(?)"]
+fruits.append("green watermelon")
+print(fruits[4:])
