664bc223de246ca7ef1b4971230d71e51ab2c9f9
Jiwoong <noheunhye1006@gmail.com>
2023-05-12 19:51:37 +0900

    숙제

---
+++ Jiwoong/python_HW/day9_hw.py
@@ -0,0 +1,26 @@
+# fruits=[1,2,3,4,5,6,7,8,9,10]
+# fruits.pop()
+# fruits. sort()
+# fruits.append("7")
+# print(fruits[4:7])
+
+#문제 1.바나나와 오렌지만 나오게 하시요.
+fruits=["banana","orange","apple","peach"]
+fruits.pop()
+fruits. sort()
+fruits.append("watermelon")
+print(fruits[1:3])
+
+#문제 2.오렌지와 멜론만 나오게 하시요
+fruits=["banana","orange","apple","peach"]
+fruits.pop()
+fruits. sort()
+fruits.append("watermelon")
+print(fruits[2:4])
+
+#문제 3.바나나만 나오게 하시요.
+fruits=["banana","orange","apple","peach"]
+fruits.pop()
+fruits. sort()
+fruits.append("watermelon")
+print(fruits[1:2])
