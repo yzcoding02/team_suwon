9aad085e2220963c74f6c0d3961290a5cff74394
CLaireChoi1209 <4clairechoi@gmail.com>
2023-05-13 13:25:37 +0900

    숙즈에; 유지웅 절떄 복사 금지!!

---
+++ Onyu/python_homework/day11_hw.py
@@ -0,0 +1,11 @@
+fruits = {"orange": 50, "apple": 100, "banana": 200}
+for i in range(5):
+    fruit = input("This is fruit store. What do you want?:")
+    if fruit in fruits:
+        print(f"we have {fruit}. {fruits[fruit]} left!")
+    else:
+        print(f"Sorry, we don't have {fruit}")
+
+'''
+*유지웅 저얼떄 복사 금지 저작권:온유 저작권 돈:9999*99999*9999*99999999억원(복사하면 내야함)*
+'''
